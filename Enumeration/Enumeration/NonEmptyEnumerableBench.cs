f0c758249cba8e011b6c06b15e4e078b1765e1aa
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-09-22 01:08:14 +0200

    Add nonempty enumerable case

---
+++ Enumeration/Enumeration/NonEmptyEnumerableBench.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace Enumeration
+{
+    [ReturnValueValidator]
+    public class NonEmptyEnumerableBench
+    {
+        [Benchmark(Baseline = true)]
+        public int WithoutInterface()
+        {
+            var sum = 0;
+            foreach (var i in new StructWithoutInterface())
+                sum += i;
+            return sum;
+        }
+
+        [Benchmark]
+        public int WithInterfaces()
+        {
+            var sum = 0;
+            foreach (var i in new StructWithInterfaces())
+                sum += i;
+            return sum;
+        }
+
+        public struct StructWithoutInterface
+        {
+            private int _i;
+            public StructWithoutInterface GetEnumerator() => new StructWithoutInterface();
+            public int Current => _i;
+
+            public bool MoveNext()
+            {
+                if (_i >= 10)
+                    return false;
+
+                ++_i;
+                return true;
+            }
+        }
+
+        public struct StructWithInterfaces : IEnumerable<int>, IEnumerator<int>
+        {
+            private int _i;
+            public StructWithInterfaces GetEnumerator() => new StructWithInterfaces();
+            IEnumerator<int> IEnumerable<int>.GetEnumerator() => GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+            public int Current => _i;
+            object IEnumerator.Current => _i;
+
+            public bool MoveNext()
+            {
+                if (_i >= 10)
+                    return false;
+
+                ++_i;
+                return true;
+            }
+
+            public void Reset()
+            {
+                _i = 0;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
--- Enumeration/README.md
+++ Enumeration/README.md
@@ -1,6 +1,8 @@
 ﻿# Enumeration
 
-Compares stuff related to IEnumerable<T>
+Compares stuff related to `IEnumerable<T>`.
+
+NOTE: `EmptyEnumerableBench` - both cases generate the same assembly, everything is optimized out, and BenchmarkDotNet has issues comparing empty cases.
 
 ## EmptyEnumerableBench
 
@@ -18,4 +20,21 @@ Intel Core i7-7700K CPU 4.20GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical cor
 |----------------- |----------:|----------:|----------:|-------:|---------:|
 | WithoutInterface | 0.1238 ns | 0.0077 ns | 0.0072 ns |   1.00 |     0.00 |
 |   WithInterfaces | 0.0976 ns | 0.0059 ns | 0.0055 ns |   0.79 |     0.06 |
+
+## NonEmptyEnumerableBench
+
+``` ini
+
+BenchmarkDotNet=v0.11.1, OS=Windows 10.0.17134.285 (1803/April2018Update/Redstone4)
+Intel Core i7-7700K CPU 4.20GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical cores
+.NET Core SDK=2.1.402
+  [Host]     : .NET Core 2.1.4 (CoreCLR 4.6.26814.03, CoreFX 4.6.26814.02), 64bit RyuJIT
+  DefaultJob : .NET Core 2.1.4 (CoreCLR 4.6.26814.03, CoreFX 4.6.26814.02), 64bit RyuJIT
+
+
+```
+|           Method |     Mean |     Error |    StdDev | Scaled |
+|----------------- |---------:|----------:|----------:|-------:|
+| WithoutInterface | 3.316 ns | 0.0123 ns | 0.0109 ns |   1.00 |
+|   WithInterfaces | 3.352 ns | 0.0232 ns | 0.0217 ns |   1.01 |
 
